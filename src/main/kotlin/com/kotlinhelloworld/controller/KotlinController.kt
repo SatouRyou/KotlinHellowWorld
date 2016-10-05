61fa407c955c45ace87aa69b312d01f7bbc36bcd
SatouRyou <satouryou1205@gmail.com>
2016-10-05 12:12:46 +0900

    importの整理

--- src/main/kotlin/com/kotlinhelloworld/controller/KotlinController.kt
+++ src/main/kotlin/com/kotlinhelloworld/controller/KotlinController.kt
@@ -1,7 +1,6 @@
 package com.kotlinhelloworld.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
